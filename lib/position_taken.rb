1da6561973ebdd26fc61efb5d1a1404789441850
GG Learn <ozolsdeko@gmail.com>
2017-10-27 07:32:54 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   field = board[index]
-  if field == " " || field == ""
+  if field == " " || field == "" || nil
     false
   else
     true
